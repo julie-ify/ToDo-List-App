73418767c97de7257dd05aa5decb9936b5b6fd2f
Juliana Ifionu <julieifionu@gmail.com>
2021-08-17 12:43:34 +0100

    Create html markup with ul element and style it in style.css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+<li class="list" draggable="true">
+            <div class="div1">
+              <input
+                type="checkbox"
+                name="check1"
+                id="checkbox-1"
+                class="check"
+              />
+              <label class="label">Eat food</label>
+              <span class="dot"><i class="fas fa-ellipsis-v"></i></span>
+            </div>
+            <div class="div2">
+              <input
+                type="checkbox"
+                name="check1"
+                id="checkbox-1"
+                class="check"
+              />
+              <label class="label">Eat food</label>
+              <span class="delete"><i class="far fa-trash-alt"></i></span>
+            </div>
+          </li>
 
- // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const toDoList = [
+  {
+    description: 'cook',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'dance',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'sing',
+    completed: false,
+    index: 2
+  }
+]        
 
-  return element;
+
+function populateList() {
+  let toDoListItems = toDoList;
+  for(let i = 0; i < toDoListItems.length; i += 1) {
+    let task = toDoListItems[i];
+    
+  }
 }
-
-document.body.appendChild(component());
-index.html;
