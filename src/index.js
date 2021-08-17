18a53f669db47bf33d12f0bf47f74885f9dc5845
Juliana Ifionu <julieifionu@gmail.com>
2021-08-17 15:54:02 +0100

    Add the UI features of the app

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 import _ from 'lodash';
 import './style.css';
 
-const listContainer = document.querySelector('.container')
+const listContainer = document.querySelector('.container');
 const toDoList = [
   {
     description: 'cook',
     completed: false,
-    index: 0
+    index: 1,
   },
   {
     description: 'dance',
     completed: false,
-    index: 1
+    index: 2,
   },
   {
     description: 'sing',
     completed: false,
-    index: 2
-  }
-]        
-
+    index: 3,
+  },
+];
 
 function populateList() {
   let toDoListItems = toDoList;
-  for(let i = 0; i < toDoListItems.length; i += 1) {
-    let task = toDoListItems[i];
-    let list = document.createElement('li');
+  for (let i = 0; i < toDoListItems.length; i += 1) {
+    const task = toDoListItems[i];
+    const list = document.createElement('li');
     list.classList.add('list');
-    list.id = task.id;
+    list.id = task.index;
     list.draggable = true;
-    let listFChild = document.createElement('div');
+    const listFChild = document.createElement('div');
     listFChild.classList.add('div1');
     const input = document.createElement('input');
     input.classList.add('check');
     input.type = 'checkbox';
-    input.name = 'check1'
+    input.name = 'check1';
     const label = document.createElement('label');
+    label.contentEditable = true;
     label.classList.add('label');
     label.innerHTML = task.description;
     const span = document.createElement('span');
@@ -43,14 +43,13 @@ function populateList() {
     const fontAwesome = document.createElement('i');
     fontAwesome.className += 'fas fa-ellipsis-v';
     span.appendChild(fontAwesome);
-    list.appendChild(listFChild)
-    listFChild.appendChild(input)
-    listFChild.appendChild(label)
-    listFChild.appendChild(span)
-    listContainer.appendChild(list)
+    list.appendChild(listFChild);
+    listFChild.appendChild(input);
+    listFChild.appendChild(label);
+    listFChild.appendChild(span);
+    listContainer.appendChild(list);
   }
 }
-
 
 listContainer.addEventListener('click', addNewTask);
 function addNewTask(e) {
@@ -64,4 +63,5 @@ function addNewTask(e) {
     }
   }
 }
+
 window.onload = populateList;
