0dab8dd7f27be18c3d8fc4f9146b046d52643e49
Juliana Ifionu <julieifionu@gmail.com>
2021-08-18 12:03:50 +0100

    Edit README.md

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/ToDo-List-App/",
     clean: true,
   },
 
